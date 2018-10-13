baf1c8dc599d82d0855efe183e3c1e54a0380a7d
Nguyen Dong <nguyendong4694@gmail.com>
2018-10-13 12:14:48 +0700

    modifi

--- FirstGit.java
+++ FirstGit.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class FirstGit{
-	public static void main(String args[]){
-		System.print.out("Hello World");
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.print("Hello World I am " + name);
 	}
 }
