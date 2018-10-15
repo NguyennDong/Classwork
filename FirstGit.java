481665589627197c37ffa748789f757b1cd000a3
Nguyen Dong <nguyendong4694@gmail.com>
2018-10-15 08:46:02 +0700

    modify

--- FirstGit.java
+++ FirstGit.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FirstGit{
 	public static void main(String []args){
-		Scanner sc = new Scanner(System.in);
-		String name = sc.nextLine();
-		System.out.print("Hello World I am " + name);
+		try{
+			FileInputStream fis = new FileInputStream("d:/file/mydata.bin");
+			DataInputStream dis = new DataInputStream(fis);
+			String name = dis.readLine();
+			fis.close();
+			dis.close();
+			System.out.print("Hello World I am " + name);
+		}
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 }
